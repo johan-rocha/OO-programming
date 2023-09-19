56bc1135a0b4fa7e3bd3c7a3e94439ea9000dc8a
Johan Rocha <104279524+johan-rocha@users.noreply.github.com>
2023-09-19 10:00:15 +0000

    retangulo

--- Entrega_1/universe.cpp
+++ Entrega_1/universe.cpp
@@ -20,6 +20,14 @@ class Ponto
 
         void showCoordinates();
         float distanciaDestePonto(int other_x, int other_y);
+        int getX()
+        {
+            return x;
+        }
+        int getY()
+        {
+            return y;
+        }
 
         ~Ponto(){}
 };
@@ -34,26 +42,26 @@ float Ponto::distanciaDestePonto(int other_x, int other_y)
     return abs(sqrt(pow(other_x - x, 2) + pow(other_y - y, 2)));
 }
 
-class Reta : public Ponto
+class Reta : private Ponto
 {
     private:
 
-        Reta(int x_1, int y_1, int x_2, int y_2) : Ponto(x_1, y_1) //revisar
-        {
-            Ponto ponto_1(x_1, y_1);
-            Ponto ponto_2(x_2, y_2);
-        }
+        Ponto ponto_1;
+        Ponto ponto_2;
+        
+    public:
+
+
+        Reta(int x_1, int y_1, int x_2, int y_2) : Ponto(x_1, y_1), ponto_1(x_1, x_2), ponto_2(x_2, y_2){} //rever como funciona esse contrutor
 
         float comprimentoReta();
 
         ~Reta(){}
-    public:
-
 };
 
 float Reta::comprimentoReta()
 {
-    return 
+    return ponto_1.distanciaDestePonto(ponto_2.getX(), ponto_2.getY());
 }
 
 int main()
