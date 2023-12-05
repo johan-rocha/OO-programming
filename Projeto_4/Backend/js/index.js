810099bbe2b89a8daa982065d4ad891d63070c0b
Johan Rocha <johanmarcos50@gmail.com>
2023-12-05 18:12:10 -0300

    new

---
+++ Projeto_4/Backend/js/index.js
@@ -0,0 +1,8 @@
+document.getElementById("texto").innerHTML = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore sit dolorem dolorum soluta eligendi cupiditate at quo amet. Quis aliquam a est, dignissimos ex minus vel et ab velit debitis";
+alert("piracanjuba");
+console.log("Que isso novinha, que isso");
+
+let a, b, c;
+a=5;
+b=6;
+c=7;
